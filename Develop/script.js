bfdf54bed31f4246e30644b3c995919e05fe1be7
mbyford49 <mbyford49@gmail.com>
2024-02-21 22:36:56 -0800

    character array's

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,21 @@
-// Assignment code here
+// Array's of characters available for passwords
+var uppercaseletters = [
+  "A","B","C","D","E","F","G","H","I","J","K","L","M","N",
+  "O","P","Q","R","S","T","U","V","W","X","Y","Z",
+];
+
+var lowercaseletters = [
+  "a","b","c","d","e","f","g","h","i","j","k","l","m","n",
+  "o","p","q","r","s","t","u","v","w","x","y","z",
+];
+
+var numbers = ["0","1","2","3","4","5","6","7","8","9",];
+
+var specialcharacters = [
+  "!","@","#","$","%","^","&","*","(",")","-","_","=","+",
+  "[","{","]","}",";",":","'",",","<",".",">","/","?","|",
+];
+
 
 
 // Get references to the #generate element

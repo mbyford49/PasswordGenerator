6c817ed1b1137d42d43961cd9b9fb9f0cb55194b
mbyford49 <mbyford49@gmail.com>
2024-02-27 00:01:21 -0800

    final product

--- Develop/script.js
+++ Develop/script.js
@@ -15,24 +15,19 @@ var specialcharacters = [
   "!","@","#","$","%","^","&","*","(",")","-","_","=","+",
   "[","{","]","}",";",":","'",",","<",".",">","/","?","|",
 ];
-
+// Function starts a series of prompts where the user chooses the makeup of their password, begining with password length
 function getPasswordOptions() {
   var length = parseInt(
     prompt("How many characters will your password contain")
   );
 
-  if (length < 8) {
-    alert("Password must contain at least 8 characters");
-
-    return prompt;
-  }
-
-  if (length > 128) {
-    alert("Password must contain no more than 128 characters");
-    return prompt;
-  }
-
-  var hasuppercaseletters = confirm("Click OK to include uppercase letters");
+// If statement requiring password length to be between 8 and 128 characters  
+  if (length < 8||length>128) {
+    alert("Password must contain between 8 and 128 characters");
+    writePassword()
+// Else statement providing character choices, given password length is acceptable    
+  }else{
+    var hasuppercaseletters = confirm("Click OK to include uppercase letters");
 
   var haslowercaseletters = confirm("Click OK to include lowercase letters");
 
@@ -49,7 +44,7 @@ function getPasswordOptions() {
     alert("Password must contain at least one character type");
     return prompt;
   }
-
+// Object created to store users choices that make up their password
   var passwordOptions = {
     length: length,
     hasuppercaseletters: hasuppercaseletters,
@@ -59,23 +54,30 @@ function getPasswordOptions() {
   };
 
   return passwordOptions;
-}
+  }
 
+}
+// Function for getting a random element from an array
 function getRandom(array) {
   var randIndex = Math.floor(Math.random() * array.length);
   var randElement = array[randIndex];
   return randElement;
 }
-
+// Function to generate a password based on the users choices
 function generatePassword() {
   var options = getPasswordOptions();
 
+// Empty variable to store password while being concatenated  
   var result = [];
 
+// Empty array to store all possible characters based on users choices
   var possibleCharacters = [];
 
+// Array containing one of each chosen character, making sure each chosen character is used
   var guaranteedCharacters = [];
 
+// If statements that add characters to the possibleCharacters array and push one random character to the guaranteedCharacters array 
+// based on users character choices
   if (options.hasuppercaseletters) {
     possibleCharacters = possibleCharacters.concat(uppercaseletters);
     guaranteedCharacters.push(getRandom(uppercaseletters));
@@ -95,12 +97,13 @@ function generatePassword() {
     possibleCharacters = possibleCharacters.concat(specialcharacters);
     guaranteedCharacters.push(getRandom(specialcharacters));
   }
-
+// For loop to iterate over password length, making random selections from the possibleCharacters array
+// and concatenating them into the results variable
   for (var i = 0; i < options.length; i++) {
     var possibleCharacter = getRandom(possibleCharacters);
     result.push(possibleCharacter);
   }
-
+// For loop ensuring one of each guaranteed character is in the result
   for (var i = 0; i < guaranteedCharacters.length; i++) {
     result[i] = guaranteedCharacters[i];
   }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PasswordGenerator
+This project required finishing the javascript for a password generator app wherein the user is given a series of choices to create a random password.
+These choices include a password length that must be between 8 and 128 characters, as well as what types of characters the password will consist of.
+Upon choosing the makeup of their password, the user is delivered a randomly generated and ultra secure password!
